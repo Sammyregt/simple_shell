83f5221d0adbedf9179d950ea84b73295421028e
Sammyregt <adeoyesamuel100@gmail.com>
2022-09-07 23:11:07 +0000

    Header file

--- shell.h
+++ shell.h
@@ -4,8 +4,32 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <dirent.h>
+#include <limits.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+
+extern char **environ;
 
 int pid(void);
-int readline(void);
+char*show_input(void);
+void prompt(void);
+char *_strcat(char *src);
+int _strlen(char *str);
+void place(char *str);
+char *findfile(char *command);
+int compare(char *s1, char *s2);
+int _strcmpdir(char *s1, char *s2);
+int charput(char c);
+char *str_concat(char *s1, char *s2);
+int lookforslash(char *cmd);
+int compareExit(char *s1, char *s2);
+int compareEnv(char *s1, char *s2);
+void execute_proc(char **cmd);
+char **identify_string(char *parameter);
+void controlC(int sig);
+
 
 #endif
